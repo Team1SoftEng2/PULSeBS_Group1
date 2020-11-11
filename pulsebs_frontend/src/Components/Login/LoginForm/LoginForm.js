2535c04c6c241e6401e52a2387c146ea9bff14a3
E_Zonno <eleonora.zonno@studenti.polito.it>
2020-11-11 16:30:52 +0100

    PUL-42 Small fixes, added Bootstrap to project

--- pulsebs_frontend/src/Components/Login/LoginForm/LoginForm.js
+++ pulsebs_frontend/src/Components/Login/LoginForm/LoginForm.js
@@ -4,13 +4,17 @@ const loginForm = () => {
 
   return <div className= "LoginFormContainer">
     <h1>Lecture Booking</h1>
-    <h3>Welcome!<br/>Please Login or Sign Up to continue</h3>
+    <h5>Welcome!<br/>Please Login or Sign Up to continue</h5>
+    <div className="FormInputContainer">
     <label for="email">Email</label>
     <input type = "email" id= "email" name= "email"></input>
+    </div>
+    <div className= "FormInputContainer">
     <label for="email">Password</label>
     <input type = "password" id= "password" name= "password"></input>
-    <div className="FormButtonsContainer">
-    <input type= "submit" id="Login" name="Login" value="Login"></input>
+    </div>
+    <div className="FormButtonContainer">
+    <button class="btn btn-outline-primary" id="Login">Login</button>
     </div>
   </div>
 
