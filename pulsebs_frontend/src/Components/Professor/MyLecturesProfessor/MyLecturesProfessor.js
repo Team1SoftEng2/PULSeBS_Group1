fc4b42c1b6f63c315bd1d82908fa0addfac2ae53
gabrielefantini <gabrielefantini97@gmail.com>
2020-11-14 10:48:21 +0100

    PUL-37 minor adjustment on view

--- pulsebs_frontend/src/Components/Professor/MyLecturesProfessor/MyLecturesProfessor.js
+++ pulsebs_frontend/src/Components/Professor/MyLecturesProfessor/MyLecturesProfessor.js
@@ -7,13 +7,13 @@ import API from '../../../api/API';
 export default function MyLecturesProfessor(){   
 
     const [lectures, setLectures] = React.useState([]);
-    
+
     const history = useHistory();
 
     React.useEffect(() => {
         API.getLectures()
-        .then((lectures) => {
-            setLectures(lectures);
+        .then((res) => {
+            setLectures(res);
         })
         .catch((err)=> {
             if(err.status && err.status === 401)
@@ -48,36 +48,14 @@ function LectureList({lectures, ...rest}) {
     );
 }
 
-function Lecture({lectureId, courseId, room, ...rest}) {
-    
-    return (
-        <Col lg={12}>
-            <Accordion defaultActiveKey="1">
-            <Card text="center">
-                <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    <h5>{lectureId}</h5>
-                </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                    <BookingList lectureId={lectureId} {...rest} />
-                </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            </Accordion>
-        </Col>
-    );
-}
-
-function BookingList({lectureId, history, ...rest}) {
+function Lecture({lectureId, courseId, room, history, ...rest}) {
 
     const [bookings, setBookings] = React.useState([]);
 
     React.useEffect(() => {
         API.getBookings(lectureId)
-        .then((bookings) => {
-            setBookings(bookings);
+        .then((res) => {
+            setBookings(res);
         })
         .catch((err)=> {
             if(err.status && err.status === 401)
@@ -86,6 +64,32 @@ function BookingList({lectureId, history, ...rest}) {
                 console.log(err);
             });
       }, [history, lectureId]);
+
+    return (
+        <Col lg={12}>
+            <Accordion defaultActiveKey="1">
+            <Card text="center">
+                <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                <Row>
+                    <Col>{lectureId}</Col>
+                    <Col>Booked students: {bookings.length}</Col>
+                    <Col>Room: {room}</Col>    
+                </Row>
+                </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                    <BookingList lectureId={lectureId} bookings={bookings} {...rest} />
+                </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+            </Accordion>
+        </Col>
+    );
+}
+
+function BookingList({lectureId, bookings, ...rest}) {
 
     return (
         <Col>
