6a9f53a382407f4bbf98e291997bef51f75b21c0
Atabay Heydarli <heydarli.atabay@gmail.com>
2020-11-20 12:41:19 +0100

    fixed code smells

--- pulsebs_frontend/GUI_tests/login_test/loginGuiTests.sikuli/loginGuiTests.py
+++ pulsebs_frontend/GUI_tests/login_test/loginGuiTests.sikuli/loginGuiTests.py
@@ -1,7 +1,5 @@
 #Testing the Login Page for students
 click("1605711605817.png")
-#type("1605712416809.png","http://localhost:3000/")
-#type(Key.ENTER)
 wait(2)
 
 username="1605784266001.png"
@@ -9,9 +7,6 @@ pswsection="1605712212356.png"
 loginButton="1605712233274.png"
 refreshButton="1605712302688.png"
 #Login error, invalid email
-#type(email,"s21111@studenti.polito.it")
-#type(pswsection,"11111")
-
 click(loginButton)
 
 wait(1) # to be filled
@@ -20,8 +15,6 @@ wait(1.5)
 click(refreshButton)
 wait(1.5)
 #Login error, invalid password
-#type(username,"s27001@gmail.com")
-#type(pswsection,"asasasa")
 
 click(loginButton)
 
@@ -32,8 +25,6 @@ click(refreshButton)
 wait(1.5)
 # Successful Login
 
-#type(username,"s27001@gmail.com")
-#type(pswsection,"s27001")
 
 click(loginButton)
 
@@ -46,8 +37,6 @@ click(refreshButton)
 wait(1.5)
 
 #Login error, invalid email
-#type(username,"t21111@studenti.polito.it")
-#type(pswsection," ")
 
 click(loginButton)
 
@@ -57,10 +46,8 @@ wait(1.5)
 click(refreshButton)
 wait(1.5)
 #Login error, invalid password
-#type(username,"t37001@gmail.com")
-#type(pswsection,"asasasa")
 
-haclick(loginButton)
+click(loginButton)
 
 wait(1) # to be filled
 
@@ -69,8 +56,7 @@ click(refreshButton)
 wait(1.5)
 # Successful Login
 
-#type(username,"t37001@gmail.com")
-#type(pswsection,"t37001")
+
 
 click(loginButton)
 
