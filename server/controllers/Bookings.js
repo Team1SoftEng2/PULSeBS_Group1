e73596e777b7253a2634007afb8fb6943222d5af
Gabriele <gabrielefantini97@gmail.com>
2020-12-14 10:53:04 +0100

    cannot fix code smells

--- server/controllers/Bookings.js
+++ server/controllers/Bookings.js
@@ -21,6 +21,7 @@ module.exports.apiBookingsPOST = async function apiBookingsPOST(req, res) {
   const lectureId = req.body.lectureId;
   let err;
   let lecture;
+  let notification;
   [err, lecture] = await to(Lectures.getLectureById(lectureId));
   if (err) return utils.writeJson(res, {errors: [{'msg': err}]}, 500);
   if (!lecture || lecture.length === 0) return utils.writeJson(res, {errors: [{'msg': 'lecture not found'}]}, 404);
@@ -29,7 +30,7 @@ module.exports.apiBookingsPOST = async function apiBookingsPOST(req, res) {
     if (moment().isAfter(deadline)) {
       return utils.writeJson(res, {'msg': 'Unprocessable Entity'}, 422);
     } else {
-      [err] = await to(Bookings.apiBookingsPOST(req.body));
+      [err, notification] = await to(Bookings.apiBookingsPOST(req.body));
       if (err) return utils.writeJson(res, {errors: [{'msg': err}]}, 500);
       else return utils.writeJson(res, {'msg': 'Created'}, 201);
     }
@@ -37,7 +38,9 @@ module.exports.apiBookingsPOST = async function apiBookingsPOST(req, res) {
 };
 // booking delete
 module.exports.apiBookingsDelete = async function apiBookingsDelete(req, res) {
-  const err = await to(Bookings.apiBookingsDelete(req.body));
+  let err;
+  let notification;
+  [err, notification] = await to(Bookings.apiBookingsDelete(req.body));
   if (err) return utils.writeJson(res, {errors: [{'msg': err}]}, 502);
   else return utils.writeJson(res, {'msg': 'Delete'}, 202);
 };
