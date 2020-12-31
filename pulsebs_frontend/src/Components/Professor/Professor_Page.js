40f273a994f2c6647580f4f5d800cf92df442a22
E_Zonno <eleonora.zonno@studenti.polito.it>
2020-12-31 02:43:24 +0100

    PUL-94 Statistics Charts done, todo link w/ db

--- pulsebs_frontend/src/Components/Professor/Professor_Page.js
+++ pulsebs_frontend/src/Components/Professor/Professor_Page.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Route, BrowserRouter, Switch, Redirect, useHistory } from 'react-router-dom';
 import ProfessorHeader from './ProfessorHeader/ProfessorHeader';
 import HomePageCalendarProfessor from './HomePageCalendarProfessor/HomePageCalendarProfessor';
 import MyLecturesProfessor from './MyLecturesProfessor/MyLecturesProfessor';
@@ -7,19 +7,20 @@ import RegisterAttendance from './Register_Attendance/Register_Attendance';
 import StatisticsPage from './Statistics/StatisticsPage';
 import TutorialProfessor from './TutorialProfessor/TutorialProfessor';
 import './Professor_Page.css';
+import API from '../../api/API';
 
-function professorPage (props) {
+function ProfessorPage (props) {
     const authObj = props.authObj;
     let history = useHistory();
     let [courses, setCourses] = useState([]);
     let [bookings, setBookings] = useState([]);
     let [lectures, setLectures] = useState([]);
-    let [professors, setProfessors] = useState([]);
+    let [update, setUpdate] = useState(false);
 
-    useEffect( () => API.getStudentCourses(authObj.authUser)
+
+    useEffect( () => API.getTeacherLectures()
                         .then( (res) => {
-                            setCourses(res);
-                            getAllLectures(res);
+                            setLectures(res);
                             // getAllProfessors(res);
                         })
                         .catch( (err) => {
@@ -32,7 +33,8 @@ function professorPage (props) {
 
     useEffect( () => API.getBookings()
                         .then( (res) => {
-                            setBookings(res);
+                            //setBookings(res);
+                            console.log(res);
                         })
                         .catch( (err) => {
                             if (err.status && err.status === 401)
@@ -41,29 +43,6 @@ function professorPage (props) {
                                 console.log(err);
                         }), [history, update]
     );
-    
-    const getAllLectures = (coursesList) => {
-        let lecturesList = [];
-        let professorList = professors;
-        coursesList.forEach( async (course) => {
-            if(!professorList.filter((p) => p.teacherId === course.teacherId).length){
-                let professor = await API.getUser(course.teacherId);
-                professorList.push(professor);
-                setProfessors(professorList);
-            }
-            API.getLectures(course.courseId)
-                .then( (res) => {
-                    lecturesList = lecturesList.concat(res);
-                    setLectures(lecturesList);
-                })
-                .catch( (err) => {
-                    if (err.status && err.status === 401)
-                        history.push('/');
-                    else
-                        console.log(err);
-                });
-        });
-    };
 
     if(authObj.userRole !== "professor")
         return <BrowserRouter><Redirect to = "/"/></BrowserRouter>
@@ -76,7 +55,7 @@ function professorPage (props) {
                     <Route path="/professor" exact component={HomePageCalendarProfessor} />
                     <Route path="/professor/my_lectures" exact component={MyLecturesProfessor} />
                     <Route path="/professor/register_attendance" exact component={RegisterAttendance} />
-                    <Route path="/professor/statistics" exact component={ <StatisticsPage {...props} courses={courses} lectures={lectures} bookings={bookings} professors={professors}/>} />
+                    <Route path="/professor/statistics" exact component={StatisticsPage} />
                     <Route path="/professor/tutorial" exact component={TutorialProfessor} />
                 </Switch>
             </BrowserRouter>
@@ -84,4 +63,4 @@ function professorPage (props) {
     
 }
 
-export default professorPage;
+export default ProfessorPage;
--- pulsebs_frontend/src/Components/Professor/Statistics/StatisticsPage.js
+++ pulsebs_frontend/src/Components/Professor/Statistics/StatisticsPage.js
@@ -1,42 +1,102 @@
 import React, { Component } from "react";
 import { Picklist, Option } from 'react-rainbow-components';
-import Statistics from './Statistics';
-
-const weekAxis = 1;
-const monthAxis = 2;
+import { DatePicker } from 'react-rainbow-components';
+import { VictoryChart, VictoryLine, VictoryBar } from 'victory';
+import moment from "moment";
 
 class statisticsPage extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            course: '',
-            authObj: this.props.authObj,
-            courses: this.props.courses,
-            lectures: this.props.lectures,
-            bookings: this.props.bookings,
-            events: [],
-            professors: [],
-          };
-        };
-
+    state = {
+        course: { name: 'option 1', label: 'Course1' },
+        date: new Date(),
+    };
 
     render() {
         return (
             <div id='StatisticsProfessorContainer'>
-                <div>
+                <div className='StatisticsInputContainer'>
                     <Picklist
                         onChange={course => this.setState({ course })}
                         value={this.state.course}
                         label="Select a Course">
-                        <Option name="option 1" label="Experimental Building" />
-                        <Option name="option 2" label="Empire State" />
-                        <Option name="option 3" label="Central Park" />
+                        <Option name="option 1" label="Course1" />
+                        <Option name="option 2" label="Course2" />
+                        <Option name="option 3" label="Course3" />
                     </Picklist>
+                    <div>
+                        <DatePicker
+                            value={this.state.date}
+                            minDate={new Date(2019, 0, 1)}
+                            maxDate={new Date(2025, 11, 31)}
+                            label="Select a date"
+                            onChange={value => this.setState({ date: value })}
+                            locale='en-GB'
+                        />
+                    </div>
                 </div>
-                {(this.state.course === '') ? <div>
-                </div> : <Statistics />
-                }
+                <div className='ProfessorChartContainer'>
+                    <VictoryChart domainPadding={{ x: 20 }}>
+                        <VictoryBar
+                            style={{
+                                data: { stroke: "#151A4F",
+                                fill: "#151A4F",
+                            },
+                            }}
+                            data={[
+                                { x: moment(this.state.date).subtract(3, 'days').toDate(), y: 1 },
+                                { x: moment(this.state.date).subtract(2, 'days').toDate(), y: 2 },
+                                { x: moment(this.state.date).subtract(1, 'days').toDate(), y: 3  },
+                                { x: moment(this.state.date).toDate(), y: 4 },
+                                { x: moment(this.state.date).add(1, 'days').toDate(), y: 5 },
+                                { x: moment(this.state.date).add(2, 'days').toDate(), y: 6 },
+                                { x: moment(this.state.date).add(3, 'days').toDate(), y:7 }
+                            ]}
+                        />
+                    </VictoryChart>
+                </div>
+                <div className='StatisticsInputContainer'>
+                    <div className='ProfessorChartContainer'>
+                        <div>Weekly Bookings (average)</div>
+                        <VictoryChart domainPadding={{ x: 20 }}>
+                            <VictoryLine
+                                style={{
+                                    data: { stroke: "#151A4F" },
+                                    parent: { border: "1px solid #ccc" }
+                                }}
+                                data={[
+                                    { x: 'Week1', y: 2 },
+                                    { x: 'Week2', y: 3 },
+                                    { x: 'Week3', y: 5 },
+                                    { x: 'Week4', y: 4 }
+                                ]}
+                            />
+                        </VictoryChart>
+                    </div>
+                    <div className='ProfessorChartContainer'>
+                        <div>Monthly Bookings (average)</div>
+                        <VictoryChart domainPadding={{ x: 20 }}>
+                            <VictoryLine
+                                style={{
+                                    data: { stroke: "#151A4F" },
+                                    parent: { border: "1px solid #ccc" }
+                                }}
+                                data={[
+                                    { x: 'Jan', y: 2 },
+                                    { x: 'Feb', y: 3 },
+                                    { x: 'Mar', y: 5 },
+                                    { x: 'Apr', y: 4 },
+                                    { x: 'Jun', y: 15 },
+                                    { x: 'Jul', y: 2 },
+                                    { x: 'Aug', y: 3 },
+                                    { x: 'Sep', y: 5 },
+                                    { x: 'Oct', y: 4 },
+                                    { x: 'Nov', y: 15 },
+                                    { x: 'Dec', y: 2 },
+                                ]}
+                            />
+                        </VictoryChart>
+                    </div>
+                </div>
             </div>
         );
     }
