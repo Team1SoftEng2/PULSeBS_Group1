75f9867b2f8559e73532a21b4ef6c31f316168e9
E_Zonno <eleonora.zonno@studenti.polito.it>
2021-01-03 18:58:31 +0100

    PUL-95 Added some simple frontend testing

--- pulsebs_frontend/src/Components/Professor/__test__/StatisticsPage.test.js
+++ pulsebs_frontend/src/Components/Professor/__test__/StatisticsPage.test.js
@@ -3,12 +3,24 @@ import {render} from '@testing-library/react';
 import StatisticsPage from '../Statistics/StatisticsPage';
 
 const testAuthObj = { authUser: "t37001", authErr: undefined, userRole: "professor" };
+const testCourses = [{courseId: 'TEST' , name: 'TestCourse' }];
 
 test('Check if Statistics render correctly', () => {
-    const renderResult = render(<StatisticsPage />);
+    const renderResult = render(<StatisticsPage courses = {testCourses}/>);
     expect(renderResult.getByText('Select Course')).toBeInTheDocument();
+    expect(renderResult.getByText('Daily Bookings')).toBeInTheDocument();
     expect(renderResult.getByText('Weekly Bookings (average)')).toBeInTheDocument();
     expect(renderResult.getByText('Monthly Bookings (average)')).toBeInTheDocument();
-
-
+    expect(renderResult.getByText('Jan')).toBeInTheDocument();
+    expect(renderResult.getByText('Feb')).toBeInTheDocument();
+    expect(renderResult.getByText('Mar')).toBeInTheDocument();
+    expect(renderResult.getByText('Apr')).toBeInTheDocument();
+    expect(renderResult.getByText('May')).toBeInTheDocument();
+    expect(renderResult.getByText('Jun')).toBeInTheDocument();
+    expect(renderResult.getByText('Jul')).toBeInTheDocument();
+    expect(renderResult.getByText('Aug')).toBeInTheDocument();
+    expect(renderResult.getByText('Sep')).toBeInTheDocument();
+    expect(renderResult.getByText('Oct')).toBeInTheDocument();
+    expect(renderResult.getByText('Nov')).toBeInTheDocument();
+    expect(renderResult.getByText('Dec')).toBeInTheDocument();
 });
