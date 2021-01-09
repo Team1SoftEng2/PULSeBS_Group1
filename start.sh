8880d37bda7733c3058bbbfa6bc7259f47422b72
Gabriele <gabrielefantini97@gmail.com>
2021-01-09 17:38:28 +0100

    commit

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 apt update
-apt -y upgrade
-apt update
 apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-curl -sL https://deb.nodesource.com/setup_12.x | -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt -y install nodejs
 apt-get install -y npm 
 # starting server
