25b04ec9e14feac3cbed0837e5b22bbcb14ec71c
Luduamico <72265540+Luduamico@users.noreply.github.com>
2021-01-13 20:58:31 +0100

    PUL-103 Test frontend part

--- pulsebs_frontend/src/Components/SuppOfficer/__test__/SystemSetup.test.js
+++ pulsebs_frontend/src/Components/SuppOfficer/__test__/SystemSetup.test.js
@@ -1,33 +1,36 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react'
 import SystemSetup from '../SystemSetup/SystemSetup';
 
+const testAuthObj = { authUser: "so47001", authErr: undefined, userRole: "supportOfficer" };
+
 test('Button test',()=>{
-    const renderResult = render(<SystemSetup />);
+    const renderResult = render(<SystemSetup authObj={testAuthObj} />);
+    expect(renderResult.getByText('Select and Upload your file here')).toBeInTheDocument();
     expect(renderResult.getByText('Students')).toBeInTheDocument();
     expect(renderResult.getByText('Professors')).toBeInTheDocument();
     expect(renderResult.getByText('Courses')).toBeInTheDocument();
     expect(renderResult.getByText('Enrollment')).toBeInTheDocument();
     expect(renderResult.getByText('Schedule')).toBeInTheDocument();
     expect(renderResult.getByText('Upload')).toBeInTheDocument();
-    const StudentButton = renderResult.getByText('Students');
-    fireEvent.click(StudentButton);
-});
-
-test ('Check uploaded files', async t => {
-    // Here you will put test code.
-    await t
-        .setFilesToUpload('#upload-input', [
-            '../Students.csv',
-            '../Professors.csv',
-            '../Courses.csv',
-            '../Enrollment.csv',
-            '../Schedule.csv'
-        ])
-        .click('#upload-btn');
-    console.log(t);
-});
-
-
-test('Select correct file',()=>{
     
 });
+
+describe('FileUploadField', () => {
+    const renderResult = render(<SystemSetup authObj={testAuthObj} />);
+  
+    test('should render a label and a file input field', () => {
+      expect(component.find('input')).toExist();
+      expect(component.find('label')).toExist();
+    });
+    
+    it('should attach the label to the input field', () => {
+      //const id = 'upload';
+      expect(component.find('input').prop('id')).toBe('file');
+      expect(component.find('button').prop('id')).toBe('upload');
+    });
+  
+    it('should not show preview if no csv has been selected', () => {
+      expect(component.find('csv')).not.toExist();
+    });
+  });
